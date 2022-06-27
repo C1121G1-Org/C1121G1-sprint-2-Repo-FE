5fc9a1f0ba68d714e9b071ce6dae68d5a83e4f74
phananhtuan1991 <phananhtuan1712@gmail.com>
2022-06-27 20:28:49 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SearchByNameComponent} from "./component/homepage/search-by-name/search-by-name.component";
+import {LoginComponent} from "./component/security/login/login.component";
 
 
 const routes: Routes = [
   {path: '', component: SearchByNameComponent},
+  {path: 'login', component: LoginComponent},
+
 ];
 
 @NgModule({
