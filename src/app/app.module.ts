8e17ad96b6b4fb3c5a4427f47ebe718cc468764e
duytuan2001 <p.duy.t.123@gmail.com>
2022-06-28 09:18:54 +0700

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {GuestModule} from "./component/guest/guest.module";
-import {ProfileModule} from "./component/profile/profile.module";
-import {SharedModule} from "./component/shared/shared.module";
-import {WalletModule} from "./component/wallet/wallet.module";
-import {SecurityModule} from "./component/security/security.module";
+import {GuestModule} from './component/guest/guest.module';
+import {ProfileModule} from './component/profile/profile.module';
+import {SharedModule} from './component/shared/shared.module';
+import {WalletModule} from './component/wallet/wallet.module';
+import {SecurityModule} from './component/security/security.module';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
-import {ChatboxModule} from "./component/chatbox/chatbox.module";
-import {CommentModule} from "./component/comment/comment.module";
-import {FriendModule} from "./component/friend/friend.module";
-import {GuestFriendModule} from "./component/guest-friend/guest-friend.module";
-import {HomepageModule} from "./component/homepage/homepage.module";
-import {PostModule} from "./component/post/post.module";
-import {ReportModule} from "./component/report/report.module";
-import {HttpClientModule} from "@angular/common/http";
+import {ChatboxModule} from './component/chatbox/chatbox.module';
+import {CommentModule} from './component/comment/comment.module';
+import {FriendModule} from './component/friend/friend.module';
+import {GuestFriendModule} from './component/guest-friend/guest-friend.module';
+import {HomepageModule} from './component/homepage/homepage.module';
+import {PostModule} from './component/post/post.module';
+import {ReportModule} from './component/report/report.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
--- src/app/component/guest/guest-routing.module.ts
+++ src/app/component/guest/guest-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {GuestListComponent} from './guest-list/guest-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'guest-member', component: GuestListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class GuestRoutingModule { }
+export class GuestRoutingModule {
+}
