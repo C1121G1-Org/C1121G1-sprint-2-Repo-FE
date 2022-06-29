c641c9451f363569e48e55c4d7607854d129fb89
haupham96 <hau.pv.0596@gmail.com>
2022-06-29 10:08:27 +0700

    HauPV

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SearchByNameComponent} from "./component/homepage/search-by-name/search-by-name.component";
+import {ProfileModule} from "./component/profile/profile.module";
 
 
 const routes: Routes = [
-  {path: '', component: SearchByNameComponent},
+  {path: '', component: SearchByNameComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+  ProfileModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
