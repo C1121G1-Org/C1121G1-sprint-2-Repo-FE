148a976a6c49c4645af249dad4fb2a9ff6bbbaab
SonDuong-glitch <myson102698@gmail.com>
2022-06-29 15:14:20 +0700

    son push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SearchByNameComponent} from "./component/homepage/search-by-name/search-by-name.component";
+import {MoneyChargeComponent} from "./component/wallet/money-charge/money-charge.component";
+import {GuestTop100Component} from "./component/guest/guest-top100/guest-top100.component";
 
 
 const routes: Routes = [
   {path: '', component: SearchByNameComponent},
+  {path: 'wallet', component:MoneyChargeComponent},
+  {path: 'top100', component: GuestTop100Component}
 ];
 
 @NgModule({
