778897bec7e632cc0930fa8cc1d6862c65dde9d0
C1121G1-NguyenHuuLeLong <long.nguyen211996@gmail.com>
2022-06-30 15:18:09 +0700

    update

--- src/app/component/post/post.module.ts
+++ src/app/component/post/post.module.ts
@@ -6,17 +6,19 @@ import { PostListComponent } from './post-list/post-list.component';
 import {InfiniteScrollModule} from "ngx-infinite-scroll";
 import {NgxSpinnerModule} from "ngx-spinner";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CKEditorModule} from "ckeditor4-angular";
 
 
 @NgModule({
   declarations: [PostListComponent],
-    imports: [
-        CommonModule,
-        PostRoutingModule,
-        InfiniteScrollModule,
-        NgxSpinnerModule,
-        FormsModule,
-        ReactiveFormsModule
-    ]
+  imports: [
+    CommonModule,
+    PostRoutingModule,
+    InfiniteScrollModule,
+    NgxSpinnerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CKEditorModule
+  ]
 })
 export class PostModule { }
